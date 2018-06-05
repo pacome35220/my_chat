717606770f39cf62d7143f29c75c7319e2b757fb
pacome.francon <pacome.francon@epitech.eu>
2018-06-05 17:10:05 +0200

    refactore websocket\'s data

--- app.js
+++ app.js
@@ -2,30 +2,36 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var session = require('cookie-session');
 var path = require('path');
-var compression = require('compression');
 var urlencodedParser = require('body-parser').urlencoded({
 	extended: false
 });
 
-var history = [];
+function init_new_channel(name) {
+	return ({
+		channel_name: name,
+		history: [{
+			name: 'God',
+			message: 'Welcome to ' + name,
+		}],
+	});
+}
 
-app.use(compression(),
-	express.static(path.join(__dirname, 'views')),
-	session({
-		secret: 'list'
-	}),
-	function(req, res, next) {
-		if (typeof(req.session.list) == 'undefined')
-			req.session.list = [];
-		next();
-	}
-);
+function get_channel_id(name) {
+	return (discord.findIndex(discord => discord.channel_name === name));
+}
+
+function get_history(name) {
+	return (discord[get_channel_id(name)].history);
+}
+
+var discord = [init_new_channel('general')];
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function(req, res) {
 	res.render('list.ejs', {
-		list: req.session.list
+		discord: discord
 	});
 });
 
@@ -34,33 +40,36 @@ app.post('/', urlencodedParser, function(req, res) {
 });
 
 app.get('/:name', function(req, res) {
-	if (req.session.list.indexOf(req.params.name) == -1)
-		req.session.list.push(req.params.name);
+	if (get_channel_id(req.params.name) == -1)
+		discord.push(init_new_channel(req.params.name));
 	res.render('channel.ejs', {
 		channel: req.params.name
 	});
 });
 
-io.on('connection', function(socket, name) {
-	socket.on('new_client', function(name) {
-		socket.name = name;
-		socket.broadcast.emit('new_client', name);
-		socket.emit('get_messages', history);
-	});
-	socket.on('message', function(message) {
-		socket.broadcast.emit('message', {
-			name: socket.name,
-			message: message
+io.on('connection', function(socket) {
+	socket.on('new_client', function(client) { // client.name, client.channel
+		socket.name = client.name;
+		socket.broadcast.emit('new_client', client);
+		socket.emit('get_history', {
+			channel: client.channel,
+			history: get_history(client.channel)
 		});
-		history.push({
+	});
+	socket.on('message', function(data) { // data.channel, data.message
+		socket.broadcast.emit('message', {
+			channel: data.channel,
 			name: socket.name,
-			message: message
+			message: data.message
+		});
+		get_history(data.channel).push({
+			name: socket.name,
+			message: data.message
 		});
 	});
 });
 
 app.use(function(req, res, next) {
-	res.setHeader('Content-Type', 'text/plain');
 	res.redirect("/");
 });
 
--- views/main.js
+++ views/main.js
@@ -1,27 +1,39 @@
 var socket = io.connect('http://localhost:8080');
 
 var name = prompt('What\'s yout name ?');
+var channel = document.title;
 
-socket.emit('new_client', name);
-document.title = name + ' - ' + document.title;
-
-socket.on('message', function(data) {
-	put_message(data.name, data.message)
-})
-
-socket.on('get_messages', function(history) {
-	for (var i = 0; i < history.length; i++)
-		put_message(history[i].name, history[i].message);
+socket.emit('new_client', {
+	name: name,
+	channel: channel
 });
 
-socket.on('new_client', function(name) {
-	$('#zone_chat').append('<p><em>' + name + ' join the channel !</em></p>');
+document.title = name + ' - ' + document.title;
+
+socket.on('new_client', function(client) { // client.name, client.channel
+	if (client.channel == channel)
+		$('#zone_chat').append('<p><em>' + client.name + ' join the channel !</em></p>');
+})
+
+socket.on('get_history', function(data) { // data.channel, data.history
+	if (data.channel == channel)
+		for (var i = 0; i < data.history.length; i++)
+			put_message(data.history[i].name, data.history[i].message);
+});
+
+socket.on('message', function(data) { // data.channel, data.name, data.message
+	if (data.channel == channel)
+		put_message(data.name, data.message)
 })
 
 $('#chat_form').submit(function() {
 	var message = $('#message').val();
 	if (message.match(/\S/)) {
-		socket.emit('message', message);
+		socket.emit('message', {
+			message: message,
+			channel: channel
+		});
+		console.log(message + " : " + channel)
 		put_message(name, message);
 		$('#message').val('').focus();
 	}
